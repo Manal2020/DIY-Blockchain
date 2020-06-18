bc7503228b69539026692f11728de94d4c54fb2a
unknown <manal.1417.2.19.ma@gmail.com>
2020-06-18 18:18:54 +0300

    Validation module is completed

--- validation.js
+++ validation.js
@@ -11,8 +11,13 @@ const signing = require('./signing');
  *   - have been modified since signing
  */
 const isValidTransaction = transaction => {
-  // Enter your solution here
-
+  if (transaction.amount < 0) {
+    return false;
+  }
+  const toSign = transaction.source
+    +transaction.recipient
+    +transaction.amount;
+  return signing.verify(transaction.source, toSign, transaction.signature);
 };
 
 /**
@@ -22,8 +27,12 @@ const isValidTransaction = transaction => {
  *   - they contain any invalid transactions
  */
 const isValidBlock = block => {
-  // Your code here
-
+  const transactionString = block.transactions.map(m => m.signature).join('');
+  const toHash = block.previousHash + transactionString + block.nonce;
+  if (block.hash !== createHash('sha512').update(toHash).digest('hex')) {
+    return false;
+  }
+  return block.transactions.every(isValidTransaction);
 };
 
 /**
@@ -37,8 +46,20 @@ const isValidBlock = block => {
  *   - contains any invalid transactions
  */
 const isValidChain = blockchain => {
-  // Your code here
-
+  const { blocks } = blockchain;
+  if (blocks[0].previousHash !== null) {
+    return false;
+  }
+  if (blocks.slice(1).some((r, j) => r.previousHash !== blocks[j].hash)) {
+    return false;
+  }
+  if (blocks.some(r => !isValidBlock(r))) {
+    return false;
+  }
+  return blocks
+    .map(r => r.transactions)
+    .reduce((sh , ma) => sh.concat(ma), [])
+    .every(isValidTransaction);
 };
 
 /**
@@ -47,8 +68,7 @@ const isValidChain = blockchain => {
  * (in theory) make the blockchain fail later validation checks;
  */
 const breakChain = blockchain => {
-  // Your code here
-
+  blockchain.blocks[1].transactions[0].amount = 1001001001;
 };
 
 module.exports = {
